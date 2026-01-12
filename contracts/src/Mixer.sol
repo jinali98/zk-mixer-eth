37ccb7816335bfb0dad48e5fbba83a116c6ad260
jinali98 <jinalipabasara@gmail.com>
2026-01-12 08:15:31 +0530

    updated mixer code

--- contracts/src/Mixer.sol
+++ contracts/src/Mixer.sol
@@ -6,6 +6,7 @@ import {IVerifier} from "./IVerifier.sol";
 contract Mixer {
     IVerifier public immutable i_verifier;
 
+    // this is used to map wether a commitment has been added or not
     mapping(bytes32 => bool) public s_commitments;
 
     uint256 public constant DENOMINATION_AMOUNT = 0.00001 ether;
@@ -23,16 +24,16 @@ contract Mixer {
 
     /**
     @notice Deposits funds into the mixer
-    @dev This function is used to deposit funds into the mixer
-    @param _commitment The poseidon hash of the nullifier and the secret that generates offchain
+    @param _commitment The poseidon commitment of the nullifier and the secret that generates offchain
     */
     function deposit(bytes32 _commitment) external payable {
-        // verify that the commitment is not already in the merkle tree to prevent having the same commitment multiple times
+        // verify that the commitment is not already in the merkle tree to prevent having the same commitment multiple times (prevent double spending)
         require(
             !s_commitments[_commitment],
             Mixer__CommitmentAlreadyExists(_commitment)
         );
         // allow users to deposit eth and verify that the deposited amount is similar to the fixed amount
+        // each contract has a fixed denomination amount
         require(
             msg.value == DENOMINATION_AMOUNT,
             Mixer__InvalidDepositAmount(msg.value, DENOMINATION_AMOUNT)
@@ -44,7 +45,8 @@ contract Mixer {
     /**
     @notice Withdraws funds from the mixer privately
     @dev This function is used to withdraw funds from the mixer
-    @param _proof The proof of the withdrawal which proves that the user knows a valid commitment
+    @param _proof The proof of the withdrawal which proves that the user knows a valid commitment 
+    (this is generated offchain and passed into the contract during the withdrawal)
     @param _recipient The recipient of the withdrawn funds
     */
     function withdraw(uint256 _proof, address _recipient) external {
